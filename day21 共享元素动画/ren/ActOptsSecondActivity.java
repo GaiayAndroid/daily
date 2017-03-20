4bcfcdb7a5e68042d6bd70a680b9f8a5cd24ca64
RenT <feveren@163.com>
2017-03-20 22:59:57 +0800

    add ren day21

---
+++ day21 共享元素动画/ren/ActOptsSecondActivity.java
@@ -0,0 +1,15 @@
+package com.rent.mytestapplication.activityoptions;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rent.mytestapplication.R;
+
+public class ActOptsSecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_opts_second);
+    }
+}
