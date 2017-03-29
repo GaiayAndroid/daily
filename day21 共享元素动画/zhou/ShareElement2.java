50f5fa2f7d13d1aa30dadb115906d905cc2299b7
RenT <feveren@163.com>
2017-03-29 11:37:12 +0800

    day21 zhou

---
+++ day21 共享元素动画/zhou/ShareElement2.java
@@ -0,0 +1,17 @@
+package com.zc.shareelementactivity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zc on 2017/3/28.
+ */
+
+public class ShareElement2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share_element2);
+    }
+}
