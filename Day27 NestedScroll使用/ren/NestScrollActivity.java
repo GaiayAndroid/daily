cb8fa59b76942af5cb24a8f9b5fc8c2bb8f2fb26
RenT <feveren@163.com>
2017-04-10 20:58:09 +0800

    day27

---
+++ Day27 NestedScroll使用/ren/NestScrollActivity.java
@@ -0,0 +1,17 @@
+package com.rent.mytestapplication.temp;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.rent.mytestapplication.R;
+
+public class NestScrollActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_net_scroll);
+    }
+}
