04e383902dbe57ebc63b1ef77e3e8d20a203cbc4
RenT <feveren@163.com>
2017-04-12 10:41:43 +0800

    commit day32

---
+++ day31-60/day32 自定义Behavior/ren/CustomBehaviorActivity.java
@@ -0,0 +1,31 @@
+package com.rent.mytestapplication;
+
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class CustomBehaviorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_custom_behavior);
+
+        this.findViewById(R.id.dependency).setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        int y = (int) event.getRawY() - v.getMeasuredHeight() / 2;
+                        v.layout(v.getLeft(), y, v.getRight(), y + v.getMeasuredHeight());
+                        System.out.println("y: " + y + ", getMeasuredHeight: " + v.getMeasuredHeight());
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
